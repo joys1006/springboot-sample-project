780140eefb691debe15958e6a760d61c761164aa
joys1006 <joys1006@naver.com>
2021-12-07 02:01:32 +0900

    [FIX] user login  service test code

--- java-server/src/test/java/com/toy/javaserver/api/domain/user/service/UserServiceTest.java
+++ java-server/src/test/java/com/toy/javaserver/api/domain/user/service/UserServiceTest.java
@@ -3,6 +3,7 @@ package com.toy.javaserver.api.domain.user.service;
 import com.toy.javaserver.api.common.dto.request.SignInRequestDto;
 import com.toy.javaserver.api.common.dto.request.SignUpRequestDto;
 import com.toy.javaserver.api.common.dto.response.SignInResponse;
+import com.toy.javaserver.api.common.exception.ApiException;
 import com.toy.javaserver.api.common.support.BCryptPasswordEncoderSupport;
 import com.toy.javaserver.api.common.utils.sercurity.JwtTokenProvider;
 import com.toy.javaserver.api.domain.user.orm.UserOrm;
@@ -14,6 +15,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
@@ -42,7 +44,7 @@ class UserServiceTest {
     private static final String testToken = "eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0MSIsImlhdCI6MTYzODEwODMyMywiZXhwIjoxNjM4MTExOTIzfQ.GGoHv6A0acQ9CZl30PMHiBDo7OjKENkHyiKYoPv1fFA";
 
     @Test
-    @DisplayName("로그인 서비스 테스트")
+    @DisplayName("로그인 서비스 성공 케이스")
     void signIn() {
         SignInRequestDto request = new SignInRequestDto();
 
@@ -61,6 +63,29 @@ class UserServiceTest {
 
         assertThat(result).isNotNull();
         assertThat(result.getToken()).isEqualTo(this.testToken);
+    }
+
+    @Test
+    @DisplayName("로그인 서비스 실패 테스트")
+    void signInFailed() {
+        try {
+            SignInRequestDto request = new SignInRequestDto();
+
+            request.setUserId("test1");
+            request.setPassword("1235");
+
+            UserOrm userOrm = new UserOrm();
+
+            userOrm.setUserId(request.getUserId());
+            userOrm.setPassword("$2y$10$lvrwYg8h7LSMSDo/i8HrwOuR3gD/lC4oYYF7YevobQ2xw7xJ8WHsm");
+
+            when(userRepository.findByUserId(request.getUserId())).thenReturn(Optional.of(userOrm));
+
+            SignInResponse result = userService.signIn(request);
+        } catch (ApiException e) {
+            assertThat(e.getHttpStatus()).isEqualTo(HttpStatus.BAD_REQUEST);
+            assertThat(e.getMessage()).isEqualTo("패스워드가 틀립니다.");
+        }
     }
 
     @Test
